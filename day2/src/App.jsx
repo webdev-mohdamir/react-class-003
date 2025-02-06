71e8dd24aae5da1abffb0f0418e188c93333656c
Mohd Amir <webdev.mohdamir@gmail.com>
2025-02-06 14:57:01 +0530

    Day-3

--- day2/src/App.jsx
+++ day2/src/App.jsx
@@ -1,12 +1,35 @@
-import Card from "./components/Card";
-import CardTwo from "./components/CardTwo";
+// import Card from "./components/Card";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+// import CardTwo from "./components/CardTwo";
 
 const App = () => {
+  const [products, setProducts] = useState([]);
+
+  const fetchProducts = async () => {
+    const response = await fetch("https://fakestoreapi.com/products");
+    const data = await response.json();
+    setProducts(data);
+  };
+
+  useEffect(() => {
+    console.log("useEffect");
+    // if (products.length > 0) return;
+
+    fetchProducts();
+  }, [products.length]);
+
   return (
     <>
-      {/* <Card heading="This is heading" author="Komal" />
-      <Card heading="This is heading 2" /> */}
-      <CardTwo heading={"Heading one"} author={"Komal Das"} currency="UTH" />
+      {products.map((product) => (
+        <div key={product.id}>
+          <h1>{product.title}</h1>
+          <img src={product.image} width={200} alt={product.title} />
+          <h2>{product.price}</h2>
+        </div>
+      ))}
+
+      <Link to={"/about"}>about</Link>
     </>
   );
 };
